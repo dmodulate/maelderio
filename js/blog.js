6e3dc3e97d93dbf6b04c3a747edf9c0f171dd0c1
maelderio <maelderio@maelderios-MacBook-Pro.local>
2026-03-20 12:38:15 +0530

    site foundation

---
+++ js/blog.js
@@ -0,0 +1,21 @@
+const posts = [
+  { title: "First Post", file: "posts/first-post.md" }
+];
+
+const list = document.getElementById("posts");
+const content = document.getElementById("content");
+
+posts.forEach(post => {
+  const li = document.createElement("li");
+  li.innerText = post.title;
+
+  li.onclick = () => {
+    fetch(post.file)
+      .then(res => res.text())
+      .then(md => {
+        content.innerHTML = marked.parse(md);
+      });
+  };
+
+  list.appendChild(li);
+});
---
+++ posts/post_1.md
@@ -0,0 +1,11 @@
+# First Post
+
+This is your blog.
+
+You can write in Markdown.
+
+## Ideas
+
+- Generative systems
+- Audio modulation
+- Visual experiments
